f47d78ba6bcc1679d04ca54869e732b37f3ef689
snehasingh-25 <sneha251104@gmail.com>
2024-11-28 20:35:40 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
-
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/SolarSystem/', // GitHub Pages base URL
   build: {
     outDir: 'dist', // Output directory for the build
     assetsDir: 'assets', // Directory for static assets
+    rollupOptions: {
+      // Multi-page setup
+      input: {
+        main: resolve(__dirname, 'index.html'), // Entry for main page
+        planetinfo: resolve(__dirname, 'planet-info.html'), // Entry for planet info page
+      },
+    },
   },
 });
