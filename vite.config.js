db1e3fe44c0330ad3a41ca7074ba97026e16b092
snehasingh-25 <sneha251104@gmail.com>
2024-11-28 18:13:38 +0530

    Add planet-info.html to dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
+
 export default defineConfig({
-    
   base: '/SolarSystem/', // GitHub Pages base URL
   build: {
     outDir: 'dist', // Output directory for the build
